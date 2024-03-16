cf2d69bd13c0f06aa1052e18ec067ca25d8589d1
Faisal Hakimi <faisalhakimi101@gmail.com>
2024-03-16 08:44:32 -0700

    rename env vars

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const REACT_APP_HOSTNAME = process.env["REACT_APP_HOSTNAME"];
-export const REACT_APP_SERVER_URL = process.env["REACT_APP_SERVER_URL"];
+export const REACT_APP_HOSTNAME = process.env["REACT_APP_FS_HOSTNAME"];
+export const REACT_APP_SERVER_URL = process.env["REACT_APP_FS_SERVER_URL"];
